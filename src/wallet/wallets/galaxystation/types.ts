242958c4f98772b203eb1655a533e9e7acfcd32b
JeromeTGH <jerome.tomski@gmx.fr>
2024-06-22 09:03:51 +0200

    Ajout de galaxystation dans les wallets

---
+++ src/wallet/wallets/galaxystation/types.ts
@@ -0,0 +1,90 @@
+import { Keplr } from "cosmes/registry";
+
+export type Window = {
+  galaxystation?: GalaxyStation | undefined;
+};
+
+/**
+ * A subset of the GalaxyStation extension API that is injected into the `window` object.
+ *
+ * @see https://github.com/terra-money/wallet-kit/blob/79600bb096d64754160909871dfdf89944120ce8/src/%40terra-money/station-connector/index.ts#L66
+ */
+export type GalaxyStation = {
+  keplr?: Keplr | undefined;
+  connect: () => Promise<ConnectResponse>;
+  getPublicKey: () => Promise<GetPubKeyResponse>;
+  signBytes(bytes: string, purgeQueue?: boolean): Promise<SignBytesResponse>;
+  post: (tx: GalaxyStationTx, purgeQueue?: boolean) => Promise<PostResponse>;
+  sign: (tx: GalaxyStationTx, purgeQueue?: boolean) => Promise<SignResponse>;
+};
+
+export type GalaxyStationTx = {
+  chainID: string;
+  msgs: string[];
+  fee?: string;
+  memo?: string;
+};
+
+export type ConnectResponse = {
+  addresses: Record<string, string>;
+  /**
+   * Maps the coin type to the base64 encoded public key.
+   * Is `undefined` for legacy versions of the extension.
+   */
+  pubkey?:
+    | {
+        "60": string;
+        "118": string;
+        "330": string;
+      }
+    | undefined;
+};
+
+export type GetPubKeyResponse = {
+  addresses: Record<string, string>;
+  /**
+   * Maps the coin type to the base64 encoded public key.
+   * Is `undefined` for legacy versions of the extension.
+   */
+  pubkey?:
+    | {
+        "118": string;
+        "330": string;
+      }
+    | undefined;
+};
+
+export type SignBytesResponse = {
+  public_key: string;
+  signature: string;
+  recid: number;
+};
+
+export type PostResponse = {
+  code?: number | undefined;
+  raw_log: string;
+  txhash: string;
+};
+
+// Unnecessary fields are omitted for brevity
+export type SignResponse = {
+  auth_info: {
+    fee: {
+      amount: {
+        amount: string;
+        denom: string;
+      }[];
+      gas_limit: string;
+      granter: string;
+      payer: string;
+    };
+    signer_infos: {
+      mode_info: {
+        single: {
+          mode: string;
+        };
+      };
+    }[];
+  };
+  signatures: string[];
+};
